e3a4ce6a3cc24761e768807c3b1cc79ce714da7b
Leroy-laboe <mangwararaleroy@gmail.com>
2026-08-11 11:41:06 +0800

    Assemble MRI Rankings page and wire route

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { HomePage } from './pages/HomePage';
 import { MarketplacePage } from './pages/MarketplacePage';
 import { CreatorsPage } from './pages/CreatorsPage';
+import { MriRankingsPage } from './pages/MriRankingsPage';
 import { ComingSoonPage } from './pages/ComingSoonPage';
 
 function App() {
@@ -11,7 +12,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/marketplace" element={<MarketplacePage />} />
         <Route path="/creators" element={<CreatorsPage />} />
-        <Route path="/mri-rankings" element={<ComingSoonPage title="MRI Rankings" />} />
+        <Route path="/mri-rankings" element={<MriRankingsPage />} />
         <Route path="/archive" element={<ComingSoonPage title="ARTCHIVE" />} />
         <Route path="/articon" element={<ComingSoonPage title="ARTICON" />} />
         <Route path="/academy" element={<ComingSoonPage title="ARTCADEMY" />} />
